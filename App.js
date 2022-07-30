c3f2470a8276973802b8d0f8b90e8fd580ec9ca3
exersonz <86198455+exersonz@users.noreply.github.com>
2022-07-30 08:38:43 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import PickImage from "./screens/camera.js";
+
+export default class App extends React.Component
+{
+  render()
+  {
+    return <PickImage/>;
+  }
+}
